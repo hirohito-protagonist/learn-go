680a50c41da01a2eeaa3b915e4725317f92659e7
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-07 19:52:50 +0200

    hello in spanish

--- src/fundamentals/hello/hello_test.go
+++ src/fundamentals/hello/hello_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Hiro")
+		got := Hello("Hiro", "")
 		want := "Hello, Hiro"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Hiro", "Spanish")
+		want := "Hola, Hiro"
 		assertCorrectMessage(t, got, want)
 	})
 }
