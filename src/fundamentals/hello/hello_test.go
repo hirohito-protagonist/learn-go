91098867fe7239c54cee26b41c572926b8dc18ca
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-07 19:41:24 +0200

    hello parameters

--- src/fundamentals/hello/hello_test.go
+++ src/fundamentals/hello/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hiro")
-	want := "Hello, Hiro"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Hiro")
+		want := "Hello, Hiro"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
