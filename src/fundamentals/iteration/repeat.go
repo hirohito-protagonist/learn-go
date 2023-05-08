5381a815a05cee99d2ca3e1503e35181102a4b48
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-08 17:24:36 +0200

    iteration custom repeater number

--- src/fundamentals/iteration/repeat.go
+++ src/fundamentals/iteration/repeat.go
@@ -1,8 +1,12 @@
 package iteration
 
-func Repeat(ch string) string {
+func Repeat(ch string, repeatTimes int) string {
 	out := ""
-	for i := 0; i < 5; i++ {
+	times := 5
+	if repeatTimes != 0 {
+		times = repeatTimes
+	}
+	for i := 0; i < times; i++ {
 		out += ch
 	}
 	return out
--- src/fundamentals/iteration/repeat_test.go
+++ src/fundamentals/iteration/repeat_test.go
@@ -3,9 +3,17 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	t.Run("repeat character five times", func(t *testing.T) {
-		repeated := Repeat("a")
+	t.Run("repeat character five times by default", func(t *testing.T) {
+		repeated := Repeat("a", 0)
 		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat custom numebr of repeated characters", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
 		if repeated != expected {
 			t.Errorf("expected %q but got %q", expected, repeated)
 		}
@@ -14,6 +22,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 0)
 	}
 }
