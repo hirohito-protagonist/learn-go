120d085718b6faa19d1e6be08420b4e241f6ab19
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-09 17:29:40 +0200

    slices sum all

--- src/fundamentals/array_and_slices/sum_test.go
+++ src/fundamentals/array_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -24,4 +25,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
