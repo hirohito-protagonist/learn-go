1731be63b541d7542eedd4607086473fcfbadf73
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-11 17:30:46 +0200

    table driven tests

--- src/fundamentals/structs/shapes_test.go
+++ src/fundamentals/structs/shapes_test.go
@@ -22,6 +22,21 @@ func TestArea(t *testing.T) {
 		}
 	}
 
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
 		checkArea(t, rectangle, 72.0)
