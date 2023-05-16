db53e5bf4abd60dc2b7768893aab9fa7647fd19c
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-16 19:55:47 +0200

    dictionary handle errors

--- src/fundamentals/maps/dictionary_test.go
+++ src/fundamentals/maps/dictionary_test.go
@@ -3,13 +3,23 @@ package dictionary
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	t.Run("search", func(t *testing.T) {
-		dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-		got := dictionary.Search("test")
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertStrings(t, err.Error(), want)
 	})
 }
 
