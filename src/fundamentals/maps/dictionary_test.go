0eab355275e1bc703504ddcf08167f3ee6c0c3d3
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-19 17:54:27 +0200

    dictionary check exisiting entry

--- src/fundamentals/maps/dictionary_test.go
+++ src/fundamentals/maps/dictionary_test.go
@@ -32,6 +32,28 @@ func TestDictionary(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("exisitng word word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
