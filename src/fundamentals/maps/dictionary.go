75e7e6a2c998202dbc8cf1a60215bb9d29402b72
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-19 18:04:16 +0200

    dictionary update

--- src/fundamentals/maps/dictionary.go
+++ src/fundamentals/maps/dictionary.go
@@ -1,8 +1,9 @@
 package dictionary
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -29,6 +30,20 @@ func (d Dictionary) Add(key string, value string) error {
 		d[key] = value
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = value
 	default:
 		return err
 	}
