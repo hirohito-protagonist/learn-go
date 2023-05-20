0d627997031f6109ca89e6037d3c444c312125eb
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-20 19:59:00 +0200

    dependency injection more general interface

--- src/fundamentals/dependency_injection/di.go
+++ src/fundamentals/dependency_injection/di.go
@@ -1,10 +1,18 @@
 package di
 
 import (
-	"bytes"
 	"fmt"
+	"io"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
+
+// func GreetHandler(w http.ResponseWriter, r *http.Request) {
+// 	Greet(w, "world")
+// }
+
+// func main() {
+// 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(GreetHandler)))
+// }
