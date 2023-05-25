bee0a836a87c62ef8b84b415265e9fd8a1fee388
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-05-25 19:46:19 +0200

    countdown remove default sleeper

--- src/fundamentals/mocking/main.go
+++ src/fundamentals/mocking/main.go
@@ -24,12 +24,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, strconv.FormatInt(int64(i), 10))
