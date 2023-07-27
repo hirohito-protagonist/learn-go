010b4af4c4e90eba9ddfcc06fa08935b1e1a7aa1
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-07-27 18:14:27 +0200

    refactor test to be a property based

--- src/fundamentals/roman_numerals/numeral_test.go
+++ src/fundamentals/roman_numerals/numeral_test.go
@@ -4,22 +4,23 @@ import "testing"
 
 func TestRomanNumerals(t *testing.T) {
 
-	t.Run("1 get converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			want := test.Want
 
-	t.Run("2 get converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		})
+	}
 }
